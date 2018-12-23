5d1c967709ddc0859e9d0664e710062b14aa9ec2
a <ruan65@gmail.com>
2018-12-23 15:51:06 +0300

    permissions

--- app/src/main/java/org/premiumapp/arforecast/data/provider/LocationProviderImpl.kt
+++ app/src/main/java/org/premiumapp/arforecast/data/provider/LocationProviderImpl.kt
@@ -1,11 +1,41 @@
 package org.premiumapp.arforecast.data.provider
 
+import android.content.Context
+import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.Deferred
+import org.premiumapp.arforecast.R
 import org.premiumapp.arforecast.data.db.entity.WeatherLocation
 
-class LocationProviderImpl : LocationProvider {
+class LocationProviderImpl(
+    private val fusedLocationProviderClient: FusedLocationProviderClient,
+    context: Context) : PreferencesProvider(context), LocationProvider {
 
     override suspend fun hasLocationChanged(lastWeatherLocation: WeatherLocation): Boolean {
-        return true
+
+        val deviceLocationChanged = isDeviceLocationChanged()
+        return deviceLocationChanged || isCustomLocationChanged()
+    }
+
+    private fun isCustomLocationChanged(): Boolean {
+
+
+    }
+
+    private fun isDeviceLocationChanged(): Boolean {
+        if (!isUsingDeviceLocation()) return false
+
+        val deviceLocation = getDeviceLocation().await() ?: return false
+
+        val comparisonThreshold = 0.03
+    }
+
+    private fun getDeviceLocation(): Deferred<Location?> {
+        return fusedLocationProviderClient.lastLocation
+    }
+
+    private fun isUsingDeviceLocation(): Boolean {
+        return preferences.getBoolean(appContext.getString(R.string.key_use_device_location), false)
     }
 
     override suspend fun getPreferredLocation(): String {
