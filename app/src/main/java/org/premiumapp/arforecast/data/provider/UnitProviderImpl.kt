7d08ceaa8cbd056671861470234aa4d79f04cde5
a <ruan65@gmail.com>
2018-12-23 15:06:05 +0300

    PrefsProvider

--- app/src/main/java/org/premiumapp/arforecast/data/provider/UnitProviderImpl.kt
+++ app/src/main/java/org/premiumapp/arforecast/data/provider/UnitProviderImpl.kt
@@ -1,20 +1,14 @@
 package org.premiumapp.arforecast.data.provider
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
 import org.premiumapp.arforecast.R
 import org.premiumapp.arforecast.internal.UnitSystem
 
-class UnitProviderImpl(context: Context) : UnitProvider {
-
-    private val appContext = context.applicationContext
-
-    private val preferences: SharedPreferences
-        get() = PreferenceManager.getDefaultSharedPreferences(appContext)
+class UnitProviderImpl(context: Context) : PreferencesProvider(context), UnitProvider {
 
     override fun getUnitSystem(): UnitSystem {
-        val selectedName = preferences.getString(appContext.getString(R.string.key_unit_system), UnitSystem.METRIC.name)
+        val selectedName = preferences.getString(
+            appContext.getString(R.string.key_unit_system), UnitSystem.METRIC.name)
         return UnitSystem.valueOf(selectedName!!)
     }
 }
